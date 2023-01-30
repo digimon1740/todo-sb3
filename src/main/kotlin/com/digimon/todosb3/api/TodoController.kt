ea00ddd7035be9dcb5bfc82c7dc34e1c9dc069e2
상훈 <sanghoon@cham-ui-MacBookAir.local>
2023-01-30 23:09:17 +0900

    Virtual Thread Hello World

--- src/main/kotlin/com/digimon/todosb3/api/TodoController.kt
+++ src/main/kotlin/com/digimon/todosb3/api/TodoController.kt
@@ -1,11 +1,27 @@
 package com.digimon.todosb3.api
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
 
 @RestController
 @RequestMapping("/v1/api/todos")
 class TodoController {
 
 
+    @GetMapping
+    fun hello(): String {
+        val response = Executors.newVirtualThreadPerTaskExecutor().use { executor ->
+            val future = executor.submit(Callable {
+                "Hello"
+            })
+            val future2 = executor.submit(Callable {
+                "World!"
+            })
+            listOf(future, future2)
+        }
+        return response.joinToString { it.get() }
+    }
 }
